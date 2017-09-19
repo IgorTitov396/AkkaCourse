9e28886b6d5e78169b91b739e6e2dad97e9d71ed
Igor Titov <i.titov1@tinkoff.ru>
2017-09-19 15:12:27 +0300

    First task v1.1

---
+++ src/test/scala/program/actors/ActorForFirstTaskSpec.scala
@@ -0,0 +1,24 @@
+package program.actors
+
+import akka.actor.ActorSystem
+import akka.testkit.TestActorRef
+import org.scalatest.{BeforeAndAfterEach, FunSpecLike, Matchers}
+import program.actors.messages.MessageForFirstTask
+
+class ActorForFirstTaskSpec extends FunSpecLike with Matchers with BeforeAndAfterEach {
+  implicit val system = ActorSystem()
+  describe("\nActorForFirstTask") {
+    describe("given StringRequest") {
+      it("should save last string") {
+        val actorRef = TestActorRef(new ActorForFirstTask())
+        actorRef ! MessageForFirstTask("Test 1")
+        val actorThatReciveMessage = actorRef.underlyingActor
+        actorThatReciveMessage.str should equal("Test 1")
+        actorRef ! MessageForFirstTask("Test 2")
+        actorThatReciveMessage.str should(equal("Test 2"))
+        actorRef ! "str"
+        actorThatReciveMessage.str should(equal("Test 2"))
+      }
+    }
+  }
+}
